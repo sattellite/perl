a206d92eab4fba6acc47c4c487ec45a55afaeec8
sattellite <sattellite@yandex.com>
2010-08-01 11:07:31 +0400

    Update license

--- parser/parser.pl
+++ parser/parser.pl
@@ -2,7 +2,31 @@
 # Скрипт для парсинга RSS ленты формируемой с помощью модуля RSS для phpBB форума/трекера
 # http://naklon.info/rss/about.htm
 # Author: sattellite
+# E-Mail: sattellite[at]bks-tv.ru
 # License: MIT
+
+#   The MIT License
+#
+#   Copyright (c) 2010 sattellite
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#   THE SOFTWARE.
+
 use strict;
 use warnings;
 use utf8;
@@ -35,7 +59,7 @@ foreach my $item ( @{$rai->items} ) {
 ## Подпрограммы
 
 ## Создание массива, который все содержимое разбивает в элементы по строкам
-sub pd 
+sub pd
 {
     my ( $str ) = @_;
     my @content = split(/\n/, $str);
