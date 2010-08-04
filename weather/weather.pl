69812ab99c052026af219c5abd53e7599b8aceb3
sattellite <sattellite@yandex.ru>
2010-08-05 00:53:15 +0400

    modified:   README 	modified:   weather/weather.pl

--- weather/weather.pl
+++ weather/weather.pl
@@ -6,21 +6,15 @@ use strict;
 use open qw(:utf8 :std);
 use utf8;
 
-weather_comm();
-
-sub weather_comm {
-    my ($msg, $city) = @_;
-    $city = $ARGV[0]; $city = 'Брянск' unless $city;
-    my $w = new Weather::Google( $city, {language => 'ru'} );
-    my @wt = $w->current qw( condition temp_c humidity wind_condition );
-    my @wh = $w->tomorrow qw( condition high );
-    my $weather = "Погода предоставлена Google\n\n"."Сейчас в городе $city за окном:\n$wt[0]\n".
-    "Температура: $wt[1] °C\n$wt[2]\n$wt[3]\n\nЗавтра:\n$wh[0]\nТемпература: $wh[1] °C\n";
-    unless (scalar @{$w->forecast}) {
-        print "Тю\n";
-        return;
-    }
-    print $weather;
-    
-#    send_message($msg, $weather);
+my ($msg, $city) = @_;
+$city = $ARGV[0]; $city = 'Брянск' unless $city;
+my $w = new Weather::Google( $city, {language => 'ru'} );
+my @wt = $w->current qw( condition temp_c humidity wind_condition );
+my @wh = $w->tomorrow qw( condition high );
+my $weather = "Погода предоставлена Google\n\n"."Сейчас в городе $city за окном:\n$wt[0]\n".
+"Температура: $wt[1] °C\n$wt[2]\n$wt[3]\n\nЗавтра:\n$wh[0]\nТемпература: $wh[1] °C\n";
+unless (scalar @{$w->forecast}) {
+    print "Тю\n";
+    return;
 }
+print $weather;
